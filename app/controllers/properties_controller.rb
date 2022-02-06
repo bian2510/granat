c0acf917a5573b122b7362cd774da8290108bfcb
Fabian Hernandez <fabian@covr.care>
2022-02-06 20:59:05 -0300

    restrict links

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,6 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: %i[ edit update destroy ]
 
   # GET /properties or /properties.json
   def index
